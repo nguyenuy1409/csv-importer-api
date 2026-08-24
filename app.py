71b97e3ba512c3b034a80d7640d26af51cd7ebde
nguyenuy1409 <nguyenuyy1409@gmail.com>
2026-08-24 17:19:32 +1000

    complete csv importer api with sqlite

---
+++ app.py
@@ -0,0 +1,131 @@
+import csv
+import io
+import sqlite3
+from flask import Flask, jsonify, request
+from database import get_connection
+
+app = Flask(__name__)
+
+@app.route("/import", methods=["POST"])
+def import_csv():
+    # Check if the request includes a file
+    if "file" not in request.files:
+        return jsonify({"error": "Please attach CSV file with key 'file'"}), 400
+
+    file = request.files["file"]
+
+    # Check if user choose empty file
+    if file.filename == "":
+        return jsonify({"error": "No files have been selected for upload yet"}), 400
+
+    # Read file CSV from memory (no need to save to the hard drive)
+    try:
+        stream = io.StringIO(file.stream.read().decode("utf-8"), newline=None)
+        csv_reader = csv.DictReader(stream)
+    except Exception as e:
+        return jsonify({"error": f"Can not read CSV file: {str(e)}"}), 400
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # Initialize counter variables to statistically analyze the results
+    total_rows = 0
+    imported_count = 0
+    invalid_rows = []
+    duplicate_rows = []
+
+    # Temporary list to catch duplicates directly within the CSV file
+    seen_emails_in_file = set()
+
+    # Trarvesal every lines in CSV file
+    for row_idx, row in enumerate(csv_reader, start=1):
+        total_rows += 1
+        name = row.get("name", "").strip() if row.get("name") else ""
+        email = row.get("email", "").strip() if row.get("email") else ""
+        age_raw = row.get("age", "").strip() if row.get("age") else ""
+
+        # Check invalid rows
+        # Check for missing name or email
+        if not name or not email:
+            invalid_rows.append({"row": row_idx, "data": row, "reason": "Missing name or email"})
+            continue
+
+        # Check data type of age
+        age = None
+        if age_raw:
+            try:
+                age = int(age_raw)
+                if age < 0 or age > 120:
+                    invalid_rows.append({"row": row_idx, "data": row, "reason": "Invalid age (0-120)"})
+                    continue
+            except ValueError:
+                invalid_rows.append({"row": row_idx, "data": row, "reason": "Age must be Integer"})
+                continue
+
+        # Check for duplicate rows
+        # Check for duplicate with the previous line directly in the CSV file     
+        if email in seen_emails_in_file:
+            duplicate_rows.append({"row": row_idx, "data": row, "reason": "Email duplicated in CSV file"})
+            continue
+
+        # Check for duplicates with the existing data in SQLite
+        cursor.execute("SELECT id FROM users WHERE email = ?", (email,))
+        if cursor.fetchone() is not None:
+            duplicate_rows.append({"row": row_idx, "data": row, "reason": "Email is already existed in Database"})
+            continue      
+
+        # INSERT: Add valid line to SQLite
+        try:
+            cursor.execute(
+                "INSERT INTO users (name, email, age) VALUES (?, ?, ?)",
+                (name, email, age)
+            )
+            seen_emails_in_file.add(email)
+            imported_count += 1
+        except sqlite3.IntegrityError as e:
+            duplicate_rows.append({"row": row_idx, "data": row, "reason": str(e)})
+
+    # Save all the changes to DB and close connection
+    conn.commit()
+    conn.close()
+
+    # Return detailed JSON result
+    return jsonify({
+        "message": "The import process is complete",
+        "summary": {
+            "total_rows": total_rows,
+            "imported": imported_count,
+            "invalid_count": len(invalid_rows),
+            "duplicate_count": len(duplicate_rows)
+        },
+        "invalid_rows": invalid_rows,
+        "duplicate_rows": duplicate_rows
+    }), 200
+
+@app.route("/display", methods=["GET"])
+def display_data():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # Retrieve all the record from users table
+    cursor.execute("SELECT id, name, email, age FROM users")
+    rows = cursor.fetchall()
+    conn.close()
+
+    # Transform SQLite Row data to dictionary list
+    users_list = []
+    for row in rows:
+        users_list.append({
+            "id": row["id"],
+            "name": row["name"],
+            "email": row["email"],
+            "age": row["age"]
+        })
+    return jsonify({
+        "total_records": len(users_list),
+        "data": users_list
+    }), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+DB_NAME = "data.db"
+
+def get_connection():
+    # Function to open and return the connection to SQLite file
+    conn = sqlite3.connect(DB_NAME)
+    # Help extract date based on columns name instead of index 
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            age INTEGER
+    );
+""")
+
+    conn.commit();
+    conn.close();
+
+if __name__ == "__main__":
+    init_db()
